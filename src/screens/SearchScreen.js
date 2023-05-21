d7a2bdad6566ada1653a945dbce8f29f288bf957
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-05-21 17:33:38 +0100

    Created reusable component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import Yelp from "../api/Yelp";
 import useResults from "../hooks/useResults";
+import ResuletsList from "../components/ResuletsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
@@ -17,6 +18,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResuletsList title="Cost Effective" />
+      <ResuletsList title="Bit Pricier" />
+      <ResuletsList title="Big Spender" />
     </View>
   );
 };
